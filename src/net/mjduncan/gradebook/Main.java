e89d175d2a4b0b50a1c5378ec1153c329bea9268
Michael Duncan <mjd1702-misc@yahoo.com.au>
2020-12-15 14:47:44 +1100

    Favicon change

--- src/net/mjduncan/gradebook/Main.java
+++ src/net/mjduncan/gradebook/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("new-gradebook.fxml"));
         primaryStage.setTitle("Gradebook");
-        primaryStage.getIcons().add(new Image("\\resources\\icons\\favicon5.png"));
+        primaryStage.getIcons().add(new Image("\\resources\\icons\\favicon6.png"));
 
         GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
         int width = gd.getDisplayMode().getWidth();
